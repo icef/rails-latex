380a58302c0c7f67d3f5b76196867365b5fddc17
Daniel Meißner <d.meissner@dmei.de>
2013-10-02 13:54:21 +0200

    Fixed tests.

--- test/test_latex_to_pdf.rb
+++ test/test_latex_to_pdf.rb
@@ -27,10 +27,10 @@ class TestLatexToPdf < Test::Unit::TestCase
   end
 
   def test_escape
-    assert_equal "dsf \\textless{} \\textgreater{} \\& ! @ \\# \\$ \\% \\textasciicircum{} \\textasciitilde{} \\textbackslash{} fds", LatexToPdf.escape_latex('dsf < > & ! @ # $ % ^ ~ \\ fds')
+    assert_equal "dsf \\textless{} \\textgreater{} \\& ! @ \\# \\$ \\% \\textasciicircum{} \\textasciitilde{} \\textbackslash{} fds {[ ]}", LatexToPdf.escape_latex('dsf < > & ! @ # $ % ^ ~ \\ fds [ ]')
     LatexToPdf.instance_eval{@latex_escaper=nil}
     require 'redcloth'
-    assert_equal "dsf \\textless{} \\textgreater{} \\& ! @ \\# \\$ \\% \\^{} \\~{} \\textbackslash{} fds", LatexToPdf.escape_latex('dsf < > & ! @ # $ % ^ ~ \\ fds')
+    assert_equal "dsf \\textless{} \\textgreater{} \\& ! @ \\# \\$ \\% \\^{} \\~{} \\textbackslash{} fds [ ]", LatexToPdf.escape_latex('dsf < > & ! @ # $ % ^ ~ \\ fds [ ]')
   end
 
   def test_broken_doc_halt_on_error
@@ -38,7 +38,7 @@ class TestLatexToPdf < Test::Unit::TestCase
       LatexToPdf.generate_pdf(IO.read(File.expand_path('../test_broken_doc.tex',__FILE__)),{})
       fail "Should throw exception"
     rescue => e
-      assert(/^lualatex failed: See / =~ e.message)
+      assert_match /^xelatex failed: See/,  e.message
     end
   end
 
@@ -54,18 +54,13 @@ class TestLatexToPdf < Test::Unit::TestCase
       LatexToPdf.generate_pdf(IO.read(File.expand_path('../test_doc.tex',__FILE__)),{})
     end
     assert_equal "The last page is ??.\n\n1\n\n\f", `pdftotext #{pdf_file} -`
-
-    assert_equal ["#{TMP_DIR}/tmp/rails-latex/input.log"], Dir["#{TMP_DIR}/tmp/rails-latex/*"]
   end
 
   def test_generate_pdf_two_parse
     pdf_file=write_pdf do
-      LatexToPdf.config[:parse_twice]=true
-      LatexToPdf.generate_pdf(IO.read(File.expand_path('../test_doc.tex',__FILE__)),{})
+      LatexToPdf.generate_pdf(IO.read(File.expand_path('../test_doc.tex',__FILE__)), { parse_twice: true })
     end
     assert_equal "The last page is 1.\n\n1\n\n\f", `pdftotext #{pdf_file} -`
-
-    assert_equal ["#{TMP_DIR}/tmp/rails-latex/input.log"], Dir["#{TMP_DIR}/tmp/rails-latex/*"]
   end
 
 end
